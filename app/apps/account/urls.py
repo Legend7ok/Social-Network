9cb6d8fc36e98bc2e20e362e0be1cc5e06d6c3c8
Legend_7ok <dimtkac777@gmail.com>
2026-02-26 23:54:11 +0200

    Add change password feature

--- app/apps/account/urls.py
+++ app/apps/account/urls.py
@@ -7,5 +7,10 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
+    # Password change
+
+    path('password_change/', auth_views.PaswordChangeView, name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView, name='password_change_done'),
+
     path('', views.dashboard, name='dashboard'),
 ]
