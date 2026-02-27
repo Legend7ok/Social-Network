8d16edec8928bacda933f4da3001517c541470a0
Legend_7ok <dimtkac777@gmail.com>
2026-02-27 11:42:29 +0200

    Add register feature

--- app/apps/account/urls.py
+++ app/apps/account/urls.py
@@ -17,4 +17,6 @@ urlpatterns = [
     path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
     path('', views.dashboard, name='dashboard'),
+
+    path('register/', views.register, name='register'),
 ]
