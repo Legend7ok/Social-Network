22faf774dd766f24f611e0eeb990b1e39fc3905a
Legend_7ok <dimtkac777@gmail.com>
2026-02-27 12:39:35 +0200

    Add ProfileEditForm for editing profile model

--- app/apps/account/forms.py
+++ app/apps/account/forms.py
@@ -24,3 +24,8 @@ class UserEditForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'email']
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['date_of_birth', 'photo']
