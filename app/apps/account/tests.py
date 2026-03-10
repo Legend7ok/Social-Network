8a7644b3e86a61597deeafad4d8a4ce633583652
Legend_7ok <dimtkac777@gmail.com>
2026-03-10 13:38:33 +0200

    Add test fixture

--- app/apps/account/tests.py
+++ app/apps/account/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+import pytest
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+from .models import Profile
+
+
+@pytest.fixture
+def user(db):
+    user_model = get_user_model()
+    password = "testpass123"
+    user_obj = user_model.objects.create_user(
+        username="alice",
+        first_name="Alice",
+        email="alice@example.com",
+        password=password,
+    )
+    Profile.objects.create(user=user_obj)
+    return user_obj, password
