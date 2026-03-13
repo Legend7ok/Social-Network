aefd0ea14919cfd5c972e7e14b67a787f2297697
Legend_7ok <dimtkac777@gmail.com>
2026-03-13 20:06:23 +0200

    feat: add custom pipeline to auto-create Profile for social users

--- app/settings/settings.py
+++ app/settings/settings.py
@@ -135,6 +135,20 @@ AUTHENTICATION_BACKENDS = [
     'social_core.backends.github.GithubOAuth2',
 ]
 
+SOCIAL_AUTH_PIPELINE = [
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    # Custom pipeline
+    'apps.account.authentication.create_profile',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+]
+
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = env('SOCIAL_AUTH_GOOGLE_OAUTH2_KEY')
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = env('SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET')
 
