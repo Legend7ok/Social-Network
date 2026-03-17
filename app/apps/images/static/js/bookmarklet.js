2b7ad256dea13add287d574adc9bc68c9203d6bf
Legend_7ok <dimtkac777@gmail.com>
2026-03-17 15:08:07 +0200

    feat: add variables, urls and load static for bookmarklet

---
+++ app/apps/images/static/js/bookmarklet.js
@@ -0,0 +1,12 @@
+const siteUrl = '//127.0.0.1:8000/';
+const styleUrl = siteUrl + 'static/css/bookmarklet.css';
+const minWidth = 250;
+const minHeight = 250;
+
+// load CSS
+const head = document.getElementsByTagName('head')[0];
+const link = document.createElement('link');
+link.rel = 'stylesheet';
+link.type = 'text/css';
+link.href = styleUrl + '?r=' + Math.floor(Math.random()*9999999999999999);
+head.appendChild(link);
