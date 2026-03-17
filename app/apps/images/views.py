a66422bf8198cea95b5443265f13a5b621348251
Legend_7ok <dimtkac777@gmail.com>
2026-03-17 12:04:02 +0200

    refactor: remove unused variable cd in image_create view

--- app/apps/images/views.py
+++ app/apps/images/views.py
@@ -8,7 +8,6 @@ def image_create(request):
     if request.method == 'POST':
         form = ImageCreateForm(request.POST)
         if form.is_valid():
-            cd = form.cleaned_data
             new_image = form.save(commit=False)
             new_image.user = request.user
             new_image.save()
