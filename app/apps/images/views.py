a8169c430d779271250c4cf948f19386886d8384
Legend_7ok <dimtkac777@gmail.com>
2026-03-20 16:53:59 +0200

    feat: add image detail view

--- app/apps/images/views.py
+++ app/apps/images/views.py
@@ -1,14 +1,16 @@
 from django.conf import settings
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.http import HttpResponse
 from .forms import ImageCreateForm
+from .models import Image
 
 
 def bookmarklet_launcher(request):
     js = render(request, 'bookmarklet_launcher.js', {'site_url': settings.SITE_URL})
     return HttpResponse(js, content_type='application/javascript')
+
 
 @login_required
 def image_create(request):
@@ -30,4 +32,13 @@ def image_create(request):
             'section': 'images',
             'form': form
         }
+    )
+
+
+def image_detail(request, id, slug):
+    image = get_object_or_404(Image, id=id, slug=slug)
+    return render(
+        request,
+        'images/image/detail.html',
+        {'section': 'images', 'image': image}
     )
