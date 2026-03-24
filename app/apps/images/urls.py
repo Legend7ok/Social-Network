41e710d10e70d2e67c05c401397fd343b6306db2
Legend_7ok <dimtkac777@gmail.com>
2026-03-24 20:05:27 +0200

    feat: add like endpoint url

--- app/apps/images/urls.py
+++ app/apps/images/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('create/', views.image_create, name='create'),
     path('bookmarklet_launcher.js', views.bookmarklet_launcher, name='bookmarklet_launcher'),
     path('detail/<int:id>/<slug:slug>/', views.image_detail, name='detail'),
+    path('like/', views.image_like, name='like'),
 ]
