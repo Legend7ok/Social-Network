fcab77f5337e13e23f0a434aab9b3a956df511a0
Legend_7ok <dimtkac777@gmail.com>
2026-04-14 21:25:59 +0300

    feat(account): add user list and user detail views

--- app/apps/account/views.py
+++ app/apps/account/views.py
@@ -6,6 +6,8 @@ from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditFor
 from django.contrib.auth.decorators import login_required
 from .models import Profile
 from django.contrib import messages
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
 
 
 def user_login(request):
@@ -31,11 +33,13 @@ def user_login(request):
                   'account/login.html',
                   {'form': form})
 
+
 @login_required
 def dashboard(request):
     return render(request,
                   'account/dashboard.html',
                   {'section': 'dashboard', 'site_url': settings.SITE_URL})
+
 
 def register(request):
     if request.method == "POST":
@@ -58,6 +62,7 @@ def register(request):
                   'account/register.html',
                   {'user_form': user_form})
 
+
 @login_required
 def edit(request):
     if request.method == "POST":
@@ -79,3 +84,23 @@ def edit(request):
                   'account/edit.html',
                   {'user_form': user_form,
                    'profile_form': profile_form})
+
+
+@login_required
+def user_list(request):
+    users = User.objects.filter(is_active=True)
+    return render(
+        request,
+        'account/user/list.html',
+        {'section': 'people', 'users': users}
+    )
+
+
+@login_required
+def user_detail(request, username):
+    user = get_object_or_404(User, username=username, is_active=True)
+    return render(
+        request,
+        'account/user/detail.html',
+        {'section': 'people', 'user': user}
+    )
