1f35a87441008e516a6322d6bf96a9b8a2e6bc39
Legend_7ok <dimtkac777@gmail.com>
2026-04-17 13:36:15 +0300

    feat(actions): add generic relations to Action model

--- app/apps/actions/models.py
+++ app/apps/actions/models.py
@@ -1,13 +1,26 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 
 class Action(models.Model):
     user = models.ForeignKey('auth.User', related_name='actions', on_delete=models.CASCADE)
     verb = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
+    target_ct = models.ForeignKey(
+        ContentType,
+        blank=True,
+        null=True,
+        related_name='target_obj',
+        on_delete=models.CASCADE
+    )
+    target_id = models.PositiveIntegerField(blank=True, null=True)
+
+    target = GenericForeignKey('target_ct', 'target_id')
 
     class Meta:
         indexes = [
             models.Index(fields=['-created_at']),
+            models.Index(fields=['target_ct', 'created_at']),
         ]
         ordering = ['-created_at']
