e71ea5235782c00e3515e44a833b50dfaa53628b
Legend_7ok <dimtkac777@gmail.com>
2026-04-20 14:44:58 +0300

    feat(account): optimize queries

--- app/apps/account/views.py
+++ app/apps/account/views.py
@@ -46,7 +46,7 @@ def dashboard(request):
 
     if following_ids:
         actions = actions.filter(user_id__in=following_ids)
-    actions = actions[:10]
+    actions = actions.select_related('user', 'user__profile')[:10].prefetch_related('target')[:10]
 
     return render(
         request,
