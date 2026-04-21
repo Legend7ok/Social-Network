c2b15d5882527c11d57033e176fb3a5cf36e96b6
Legend_7ok <dimtkac777@gmail.com>
2026-04-21 20:06:34 +0300

    chore: configure debug toolbar

--- app/settings/settings.py
+++ app/settings/settings.py
@@ -42,9 +42,11 @@ INSTALLED_APPS = [
     'apps.images',
     'apps.actions',
     'easy_thumbnails',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -170,3 +172,8 @@ USE_X_FORWARDED_PORT = True
 ABSOLUTE_URL_OVERRIDES = {
     'auth.user' : lambda u: reverse_lazy('user_detail', args=[u.username]),
 }
+
+
+INTERNAL_IPS = [
+ '127.0.0.1',
+]
