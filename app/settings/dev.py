2171d53d35d2e6a8732efef624ab6d05a8708fa8
Legend_7ok <dimtkac777@gmail.com>
2026-04-24 22:07:09 +0300

    fix(dev): serve static files locally to fix debug_toolbar CORS issue

--- app/settings/dev.py
+++ app/settings/dev.py
@@ -10,5 +10,16 @@ MIDDLEWARE = ['debug_toolbar.middleware.DebugToolbarMiddleware'] + MIDDLEWARE
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+STORAGES = {
+    **STORAGES,
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
+
 hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
 INTERNAL_IPS = [ip[:ip.rfind('.')] + '.1' for ip in ips]
+
+DEBUG_TOOLBAR_CONFIG = {
+    'SHOW_TOOLBAR_CALLBACK': lambda request: True,
+}
