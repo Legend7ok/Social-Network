629ee47b3fb6b70068bb01873af56a919622e2a7
Legend_7ok <dimtkac777@gmail.com>
2026-04-25 13:50:35 +0300

    feat(docker): simplify entrypoint to migrate + exec

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-echo "Running migrations..."
 python app/manage.py migrate --noinput
 
-echo "Starting development server..."
-exec python app/manage.py runserver 0.0.0.0:8000
+exec "$@"
