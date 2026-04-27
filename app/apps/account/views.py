3f2f00fcaae5a218b827ad4e6910f18b2bf04fdf
Legend_7ok <dimtkac777@gmail.com>
2026-04-27 18:55:38 +0300

    style(account): clean up imports in views.py

--- app/apps/account/views.py
+++ app/apps/account/views.py
@@ -1,19 +1,18 @@
 from django.conf import settings
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, get_user_model
-from django.db import transaction
-from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
-from django.contrib.auth.decorators import login_required
-from .models import Profile
 from django.contrib import messages
-from django.shortcuts import get_object_or_404
-
-User = get_user_model()
+from django.contrib.auth import authenticate, login, get_user_model
+from django.contrib.auth.decorators import login_required
+from django.db import transaction
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
-from .models import Contact
+
+from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
+from .models import Profile, Contact
 from apps.actions.utils import create_action
 from apps.actions.models import Action
 from core.utils import toggle_action
+
+User = get_user_model()
 
 
 def user_login(request):
