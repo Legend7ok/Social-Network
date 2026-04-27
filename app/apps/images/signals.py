e2a59f4280b21c28b07b7a9f9bee9f909e7bf428
Legend_7ok <dimtkac777@gmail.com>
2026-04-27 18:52:23 +0300

    fix(images): replace count()+save() with atomic F() increment in signal

--- app/apps/images/signals.py
+++ app/apps/images/signals.py
@@ -1,9 +1,12 @@
+from django.db.models import F
 from django.db.models.signals import m2m_changed
 from django.dispatch import receiver
 from .models import Image
 
 
 @receiver(m2m_changed, sender=Image.users_like.through)
-def users_like_changed(sender, instance, **kwargs):
-    instance.total_likes = instance.users_like.count()
-    instance.save()
+def users_like_changed(sender, instance, action, **kwargs):
+    if action == 'post_add':
+        Image.objects.filter(pk=instance.pk).update(total_likes=F('users_like') + 1)
+    elif action == 'post_remove':
+        Image.objects.filter(pk=instance.pk).update(total_likes=F('users_like') - 1)
