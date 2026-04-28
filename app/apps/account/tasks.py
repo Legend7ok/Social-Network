c29307936aa2ddd983c22514e56795a90261ca00
Legend_7ok <dimtkac777@gmail.com>
2026-04-28 18:55:09 +0300

    feat(account): add async welcome email on registration

---
+++ app/apps/account/tasks.py
@@ -0,0 +1,19 @@
+from celery import shared_task
+from django.contrib.auth import get_user_model
+from django.core.mail import send_mail
+
+User = get_user_model()
+
+
+@shared_task
+def send_welcome_email(user_id):
+    try:
+        user = User.objects.get(id=user_id)
+        send_mail(
+            subject="Welcome to Social Network",
+            message=f"Hi {user.first_name}, thanks for joining!",
+            from_email="noreply@socialnetwork.com",
+            recipient_list=[user.email],
+        )
+    except Exception:
+        pass
