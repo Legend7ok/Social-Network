42e59039536c1c15c17dfad6131e9546411dec51
Legend_7ok <dimtkac777@gmail.com>
2026-05-04 13:52:07 +0300

    refactor(tests): merge duplicate send_welcome_email assertions into one test

--- app/apps/account/tests/test_tasks.py
+++ app/apps/account/tests/test_tasks.py
@@ -5,24 +5,15 @@ from apps.account.tasks import send_welcome_email
 
 
 @pytest.mark.django_db
-def test_send_welcome_email_sends_to_correct_recipient(user):
+def test_send_welcome_email_sends_correct_email(user):
     user_obj, _ = user
     with patch("apps.account.tasks.send_mail") as mock_send:
         send_welcome_email(user_obj.id)
 
     mock_send.assert_called_once()
     _, kwargs = mock_send.call_args
-    assert kwargs["recipient_list"] == [user_obj.email]
-
-
-@pytest.mark.django_db
-def test_send_welcome_email_uses_correct_subject(user):
-    user_obj, _ = user
-    with patch("apps.account.tasks.send_mail") as mock_send:
-        send_welcome_email(user_obj.id)
-
-    _, kwargs = mock_send.call_args
     assert kwargs["subject"] == "Welcome to Social Network"
+    assert kwargs["recipient_list"] == [user_obj.email]
 
 
 @pytest.mark.django_db
