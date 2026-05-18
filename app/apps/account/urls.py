c86b23134a565c8f7e0cc96fecad0c2858cc335a
Legend_7ok <dimtkac777@gmail.com>
2026-05-18 20:57:57 +0300

    fix(account): redirect authenticated users away from login page; prevents sidebar layout breaking on /login/

--- app/apps/account/urls.py
+++ app/apps/account/urls.py
@@ -1,7 +1,9 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path("login/", auth_views.LoginView.as_view(redirect_authenticated_user=True), name="login"),
     path("", include("django.contrib.auth.urls")),
     path("", views.home, name="home"),
     path("register/", views.register, name="register"),
