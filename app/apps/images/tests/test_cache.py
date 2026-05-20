8361227f23c38d15b5c3cc1e6de3d1faa34d7788
Legend_7ok <dimtkac777@gmail.com>
2026-05-20 16:07:48 +0300

    style: apply ruff formatting

--- app/apps/images/tests/test_cache.py
+++ app/apps/images/tests/test_cache.py
@@ -5,7 +5,6 @@ from apps.images.services import record_image_view
 from apps.images.tasks import refresh_image_ranking_cache
 
 
-
 @pytest.mark.django_db
 def test_refresh_image_ranking_cache_task_populates_cache(image, settings):
     record_image_view(image.id)
