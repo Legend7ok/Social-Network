b4b21581b8f2a13dd433d78444b85148aa0d717d
Legend_7ok <dimtkac777@gmail.com>
2026-05-27 12:49:40 +0300

    ix(celery): set prod settings as default in celery.py

--- app/config/celery.py
+++ app/config/celery.py
@@ -2,7 +2,7 @@ import os
 
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.prod")
 
 app = Celery("social_network")
 app.config_from_object("django.conf:settings", namespace="CELERY")
