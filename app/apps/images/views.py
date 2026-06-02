b88b478456719dee920db10fbd72ed79e3734ac2
Legend_7ok <dimtkac777@gmail.com>
2026-06-02 19:46:46 +0300

    feat(thumbnails): pre-generate content thumbnails async on form upload via generate_image_thumbnails.delay

--- app/apps/images/views.py
+++ app/apps/images/views.py
@@ -17,7 +17,7 @@ from .services import (
     get_images_views,
     is_first_view,
 )
-from .tasks import download_image
+from .tasks import download_image, generate_image_thumbnails
 from apps.actions.utils import create_action
 
 User = get_user_model()
@@ -141,6 +141,7 @@ def image_upload(request):
             new_image = form.save(commit=False)
             new_image.user = request.user
             new_image.save()
+            generate_image_thumbnails.delay(new_image.id)
             create_action(request.user, "uploaded image", new_image)
             messages.success(request, "Image uploaded successfully")
             return redirect(new_image.get_absolute_url())
