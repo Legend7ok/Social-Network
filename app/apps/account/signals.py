0c0c3a5edcb1715cb01330fca275f6c625c8d07d
Legend_7ok <dimtkac777@gmail.com>
2026-06-02 19:58:02 +0300

    feat(thumbnails): regenerate avatar thumbnails async on Profile.photo change via pre/post_save signals

--- app/apps/account/signals.py
+++ app/apps/account/signals.py
@@ -1,8 +1,11 @@
 from django.core.cache import cache
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 
 from apps.actions.models import Action
+
+from .models import Profile
+from .tasks import generate_avatar_thumbnails
 
 
 @receiver(post_save, sender=Action)
@@ -11,3 +14,20 @@ def invalidate_dashboard_cache(sender, instance, created, **kwargs):
         return
     for profile in instance.user.profile.followers.all():
         cache.delete(f"home_{profile.user_id}")
+
+
+@receiver(pre_save, sender=Profile)
+def remember_old_photo(sender, instance, **kwargs):
+    if not instance.pk:
+        instance._old_photo = None
+        return
+    try:
+        instance._old_photo = sender.objects.get(pk=instance.pk).photo.name
+    except sender.DoesNotExist:
+        instance._old_photo = None
+
+
+@receiver(post_save, sender=Profile)
+def generate_avatar_on_photo_change(sender, instance, **kwargs):
+    if instance.photo and instance.photo.name != instance._old_photo:
+        generate_avatar_thumbnails.delay(instance.id)
